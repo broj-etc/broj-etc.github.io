409d81804fb4e5dbd00dd5f9acee96a2d335ecf2
Antti V <johhni.games@gmail.com>
2023-10-01 21:55:13 +0300

    hotfix - add character

--- src/assets/components/components.js
+++ src/assets/components/components.js
@@ -9,6 +9,7 @@ import {
   TableCell,
   TableBody,
   InputAdornment,
+  Switch,
 } from "@mui/material";
 import * as React from "react";
 /** Variables */
@@ -44,14 +45,21 @@ const AddCharacter = ({ image, addCharacter, setAddCharacter }) => {
                 <Table>
                   <TableBody>
                     <TableRow>
-                      <TableCell colSpan={5}>
-                        <Typography variant="h3" className="h1">
+                      <TableCell colSpan={6}>
+                        <Typography variant="h1" className="h2">
                           Add Character
                         </Typography>
                       </TableCell>
                     </TableRow>
                     <TableRow>
-                      <TableCell align="center" colSpan={5}>
+                      <TableCell colSpan={6}>
+                        <Typography variant="h3" className="h1">
+                          Basic Information
+                        </Typography>
+                      </TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableCell align="center" colSpan={3}>
                         <TextField
                           id="character-name"
                           variant="filled"
@@ -59,6 +67,132 @@ const AddCharacter = ({ image, addCharacter, setAddCharacter }) => {
                           required
                           fullWidth
                         />
+                      </TableCell>
+                      <TableCell align="center" colSpan={1}>
+                        <TextField
+                          id="character-proficiency"
+                          variant="filled"
+                          label="Proficiency"
+                          InputProps={{
+                            startAdornment: (
+                              <InputAdornment position="start">
+                                +
+                              </InputAdornment>
+                            ),
+                          }}
+                          required
+                          fullWidth
+                        />
+                      </TableCell>
+                      <TableCell align="center" colSpan={1}>
+                        <TextField
+                          id="character-walking-speed"
+                          variant="filled"
+                          label="Walking Speed"
+                          InputProps={{
+                            endAdornment: (
+                              <InputAdornment position="end">
+                                ft.
+                              </InputAdornment>
+                            ),
+                          }}
+                          required
+                          fullWidth
+                        />
+                      </TableCell>
+                      <TableCell align="center" colSpan={1}>
+                        <TextField
+                          id="character-initiative"
+                          variant="filled"
+                          label="Initiative"
+                          InputProps={{
+                            startAdornment: (
+                              <InputAdornment position="start">
+                                +
+                              </InputAdornment>
+                            ),
+                          }}
+                          type="number"
+                          required
+                          fullWidth
+                        />
+                      </TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableCell align="center" colSpan={1}>
+                        <TextField
+                          id="character-Gender"
+                          variant="filled"
+                          label="Gender"
+                          required
+                          fullWidth
+                        />
+                      </TableCell>
+                      <TableCell align="center" colSpan={1}>
+                        <TextField
+                          id="character-Race"
+                          variant="filled"
+                          label="Race"
+                          required
+                          fullWidth
+                        />
+                      </TableCell>
+                      <TableCell align="center" colSpan={1}>
+                        <TextField
+                          id="character-Class"
+                          variant="filled"
+                          label="Class"
+                          required
+                          fullWidth
+                        />
+                      </TableCell>
+                      <TableCell align="center" colSpan={1}>
+                        <TextField
+                          id="character-level"
+                          variant="filled"
+                          type="number"
+                          label="Level"
+                          required
+                          fullWidth
+                        />
+                      </TableCell>
+                      <TableCell align="center" colSpan={1}>
+                        <TextField
+                          id="character-another-class"
+                          variant="filled"
+                          label="Another Class"
+                          fullWidth
+                        />
+                      </TableCell>
+                      <TableCell align="center" colSpan={1}>
+                        <TextField
+                          id="character-another-level"
+                          variant="filled"
+                          type="number"
+                          label="Level"
+                          fullWidth
+                        />
+                      </TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableCell colSpan={6}>
+                        <Typography variant="h3" className="h1">
+                          Senses
+                        </Typography>
+                      </TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableCell colSpan={6}>
+                        <Typography variant="h3" className="h1">
+                          Proficiencies & Languages
+                        </Typography>
+                      </TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableCell colSpan={6}>
+                        <Typography variant="h3" className="h1">
+                          Abilities
+                        </Typography>
                       </TableCell>
                     </TableRow>
                     <TableRow>
@@ -96,6 +230,16 @@ const AddCharacter = ({ image, addCharacter, setAddCharacter }) => {
                         <TextField
                           id="character-wisdom"
                           variant="filled"
+                          label="Wisdom"
+                          type="number"
+                          required
+                          fullWidth
+                        />
+                      </TableCell>
+                      <TableCell align="center">
+                        <TextField
+                          id="character-intelligence"
+                          variant="filled"
                           label="Intelligence"
                           type="number"
                           required
@@ -118,7 +262,11 @@ const AddCharacter = ({ image, addCharacter, setAddCharacter }) => {
                         <TextField
                           id="character-strength-bonus"
                           InputProps={{
-                            startAdornment: <InputAdornment position="start">+</InputAdornment>,
+                            startAdornment: (
+                              <InputAdornment position="start">
+                                +
+                              </InputAdornment>
+                            ),
                           }}
                           variant="filled"
                           type="number"
@@ -130,7 +278,11 @@ const AddCharacter = ({ image, addCharacter, setAddCharacter }) => {
                         <TextField
                           id="character-dexterity-bonus"
                           InputProps={{
-                            startAdornment: <InputAdornment position="start">+</InputAdornment>,
+                            startAdornment: (
+                              <InputAdornment position="start">
+                                +
+                              </InputAdornment>
+                            ),
                           }}
                           variant="filled"
                           type="number"
@@ -142,7 +294,11 @@ const AddCharacter = ({ image, addCharacter, setAddCharacter }) => {
                         <TextField
                           id="character-constitution-bonus"
                           InputProps={{
-                            startAdornment: <InputAdornment position="start">+</InputAdornment>,
+                            startAdornment: (
+                              <InputAdornment position="start">
+                                +
+                              </InputAdornment>
+                            ),
                           }}
                           variant="filled"
                           type="number"
@@ -154,7 +310,27 @@ const AddCharacter = ({ image, addCharacter, setAddCharacter }) => {
                         <TextField
                           id="character-wisdom-bonus"
                           InputProps={{
-                            startAdornment: <InputAdornment position="start">+</InputAdornment>,
+                            startAdornment: (
+                              <InputAdornment position="start">
+                                +
+                              </InputAdornment>
+                            ),
+                          }}
+                          variant="filled"
+                          type="number"
+                          required
+                          fullWidth
+                        />
+                      </TableCell>
+                      <TableCell align="center">
+                        <TextField
+                          id="character-intelligence-bonus"
+                          InputProps={{
+                            startAdornment: (
+                              <InputAdornment position="start">
+                                +
+                              </InputAdornment>
+                            ),
                           }}
                           variant="filled"
                           type="number"
@@ -166,13 +342,141 @@ const AddCharacter = ({ image, addCharacter, setAddCharacter }) => {
                         <TextField
                           id="character-charisma-bonus"
                           InputProps={{
-                            startAdornment: <InputAdornment position="start">+</InputAdornment>,
+                            startAdornment: (
+                              <InputAdornment position="start">
+                                +
+                              </InputAdornment>
+                            ),
                           }}
                           variant="filled"
                           type="number"
                           required
                           fullWidth
                         />
+                      </TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableCell colSpan={6}>
+                        <Typography variant="h3" className="h1">
+                          Saving Throws
+                        </Typography>
+                      </TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableCell align="center">
+                        <TextField
+                          id="character-strength-saving"
+                          InputProps={{
+                            startAdornment: (
+                              <InputAdornment position="start">
+                                +
+                              </InputAdornment>
+                            ),
+                          }}
+                          variant="filled"
+                          type="number"
+                          label="STR"
+                          required
+                          fullWidth
+                        />
+                        <Switch inputProps={{ "aria-label": "character-strength-saving-profiency" }} />
+                      </TableCell>
+                      <TableCell align="center">
+                        <TextField
+                          id="character-dexterity-saving"
+                          InputProps={{
+                            startAdornment: (
+                              <InputAdornment position="start">
+                                +
+                              </InputAdornment>
+                            ),
+                          }}
+                          variant="filled"
+                          type="number"
+                          label="DEX"
+                          required
+                          fullWidth
+                        />
+                        <Switch inputProps={{ "aria-label": "character-dexterity-saving-profiency" }} />
+                      </TableCell>
+                      <TableCell align="center">
+                        <TextField
+                          id="character-constitution-saving"
+                          InputProps={{
+                            startAdornment: (
+                              <InputAdornment position="start">
+                                +
+                              </InputAdornment>
+                            ),
+                          }}
+                          variant="filled"
+                          type="number"
+                          label="CON"
+                          required
+                          fullWidth
+                        />
+                        <Switch inputProps={{ "aria-label": "character-constitution-saving-profiency" }} />
+                      </TableCell>
+                      <TableCell align="center">
+                        <TextField
+                          id="character-wisdom-saving"
+                          InputProps={{
+                            startAdornment: (
+                              <InputAdornment position="start">
+                                +
+                              </InputAdornment>
+                            ),
+                          }}
+                          variant="filled"
+                          type="number"
+                          label="INT"
+                          required
+                          fullWidth
+                        />
+                        <Switch inputProps={{ "aria-label": "character-intelligence-saving-profiency" }} />
+                      </TableCell>
+                      <TableCell align="center">
+                        <TextField
+                          id="character-intelligence-saving"
+                          InputProps={{
+                            startAdornment: (
+                              <InputAdornment position="start">
+                                +
+                              </InputAdornment>
+                            ),
+                          }}
+                          variant="filled"
+                          type="number"
+                          label="WIS"
+                          required
+                          fullWidth
+                        />
+                        <Switch inputProps={{ "aria-label": "character-wisdom-saving-profiency" }} />
+                      </TableCell>
+                      <TableCell align="center">
+                        <TextField
+                          id="character-charisma-saving"
+                          InputProps={{
+                            startAdornment: (
+                              <InputAdornment position="start">
+                                +
+                              </InputAdornment>
+                            ),
+                          }}
+                          variant="filled"
+                          type="number"
+                          label="CHA"
+                          required
+                          fullWidth
+                        />
+                        <Switch inputProps={{ "aria-label": "character-charisma-saving-profiency" }} />
+                      </TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableCell colSpan={6}>
+                        <Typography variant="h3" className="h1">
+                          Skills
+                        </Typography>
                       </TableCell>
                     </TableRow>
                   </TableBody>
